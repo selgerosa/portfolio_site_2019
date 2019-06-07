9ca829f8af78f139ad3dbdafd6f1893b13c63f5d
Selina <31773266+selinagerosa@users.noreply.github.com>
2019-06-07 18:28:38 -0400

    dumb dumb got the night mode working on index

--- dark.js
+++ dark.js
@@ -1,4 +1,5 @@
-localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark'); localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
 document.addEventListener('DOMContentLoaded', (event) => {
-  ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+  ((localStorage.getItem('mode') || 'dark') === 'dark') ?
+  document.querySelector('body').classList.add('dark') :
+  document.querySelector('body').classList.remove('dark')
 })
