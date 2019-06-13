1dd8990a5c7c46c86aab6c976af5f0f80d1e1558
Selina <31773266+selinagerosa@users.noreply.github.com>
2019-06-12 23:06:12 -0400

    maybe its the eventlistener

--- dark.js
+++ dark.js
@@ -1,5 +1,12 @@
+if( document.readyState !== 'loading' ) {
+  ((localStorage.getItem('mode') || 'dark') === 'dark') ?
+document.querySelector('body').classList.add('dark') :
+document.querySelector('body').classList.remove('dark')
+}
+else {
 document.addEventListener('DOMContentLoaded', (event) => {
   ((localStorage.getItem('mode') || 'dark') === 'dark') ?
-  document.querySelector('body').classList.remove('dark') :
-  document.querySelector('body').classList.add('dark')
-})
+  document.querySelector('body').classList.add('dark') :
+  document.querySelector('body').classList.remove('dark')
+});
+}
